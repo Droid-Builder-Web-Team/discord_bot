0778fb541af08eea1721c04e77ba8932dfe758f9
dpoulson <darren.poulson@gmail.com>
2020-06-04 16:23:58 +0100

    Added 39.1% role

--- bot.js
+++ bot.js
@@ -43,8 +43,8 @@ client.on('message', message => {
 	}
 });
 
-client.on('guildMemberAdd', member => {
-	const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
+client.on('guildMemberAdd', (member) => {
+	const channel = member.guild.channels.find(ch => ch.name === 'member-log');
 	if (!channel) return;
 	channel.send(`Welcome to the server, ${member}`);
 });
