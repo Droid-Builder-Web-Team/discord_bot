c1769d36832f94b09e791f6907e9c50cd1f78e44
dpoulson <darren.poulson@gmail.com>
2020-06-04 13:57:19 +0100

    Should now use list of roles from csv

--- roles.js
+++ roles.js
@@ -1,15 +1,6 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 
-const RSERIES = '717758445956104212';
-const TSERIES = '717758451916210187';
-const BBSERIES = '717758449206820895';
-const CSERIES = '717758459080343565';
-const MSE = '717758456576081970';
-const GONK = '717758461651320864';
-const KSERIES = '717758453912830023';
-const PROTOCOL = '717758463400345681';
-
 var roles = [];
 
 fs.createReadStream(process.env.ROLE_LIST + '.csv')
@@ -29,36 +20,24 @@ module.exports = {
     if (role == null) {
       role = 'help'
     }
-    switch(role.toLowerCase()) {
-      case 'r-series':
-        message.member.addRole(RSERIES);
-        break;
-      case 't-series':
-        message.member.addRole(TSERIES);
-        break;
-      case 'bb-series':
-        message.member.addRole(BBSERIES);
-        break;
-      case 'c-series':
-        message.member.addRole(CSERIES);
-        break;
-      case 'mse':
-        message.member.addRole(MSE);
-        break;
-      case 'gonk':
-        message.member.addRole(GONK);
-        break;
-      case 'k-series':
-        message.member.addRole(KSERIES);
-        break;
-      case 'protocol':
-        message.member.addRole(PROTOCOL);
-        break;
-      case 'help':
-        message.reply("The following roles are available: ");
-        break;
-      default:
-        message.reply("Unknown role");
+    var i;
+    var assigned = 0;
+    for (i = 0; i < roles.length;i++) {
+      if (roles[i].name == role) {
+        message.member.addRole(role.id);
+        assigned = 1;
+      }
     }
+    if (assigned = 0) {
+      message.reply("Unknown role");
+    }
+    if (role == 'help') {
+      output = "The following roles are available: \n";
+      for (i = 0; i < roles.length;i++) {
+        output += "\t" + roles[i].name + "\n";
+      }
+      message.reply(output);
+    }
+
   }
 };
