57f3043224092ec9e85769fc85e0a7d09f0d7b26
dpoulson <darren.poulson@gmail.com>
2020-11-24 14:57:34 +0000

    Async rubbish

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ client.on('ready', () => {
 //			  connection.play(cast);
 //			}
 	console.log('Channels: ' + util.inspect(client.channels.fetch(greet_channel), {showHidden: false, depth: null}));
-	channel = client.channels.cache.get('715193623129489429');
+	channel = await client.channels.cache.get('715193623129489429');
 	console.log('Greet channel: ' + channel.name);
 
 	channel.send('Beep Boop');
