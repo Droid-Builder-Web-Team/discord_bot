b4550e5e244077f99baa0804954175f4b740c19e
drolsen <dolsen@connectivedx.com>
2021-02-15 23:12:38 -0800

    Adding zoom message back to welcome.js and updated Readme

--- welcome.js
+++ welcome.js
@@ -1,21 +1,13 @@
-const welcomeArray = [
-  "Welcome MEMBER_NAME, we've been expecting you",
-  "May the force be with you, MEMBER_NAME",
-  "Help me MEMBER_NAME, you're our only hope",
-  "MEMBER_NAME, we're home!",
-  "MEMBER_NAME, I'll be there for you. Cassian said I had to."
-];
-
-const welcomeZoomLink = 'Find us daily on the "N@d@" zoom channel: https://tinyurl.com/y3knc9fz';
-const welcomeHelpReminder = 'Use !help to see all my available commands.';
-
-const getRandomWelcome = (member) => welcomeArray[
-  Math.floor(Math.random() * welcomeArray.length)
-].replace(
-  'MEMBER_NAME', 
-  member.user
-);
+var welcomeArray = [
+    "Welcome MEMBER_NAME, we've been expecting you",
+    "May the force be with you, MEMBER_NAME",
+    "Help me MEMBER_NAME, you're our only hope",
+    "MEMBER_NAME, we're home!",
+    "MEMBER_NAME, I'll be there for you. Cassian said I had to."
+  ];
 
 module.exports = {
-    generateWelcome: (member) => `${getRandomWelcome(member)}\n\r${welcomeZoomLink}\n\r${welcomeReminder}`
+  generateWelcome: function() {
+    return `${welcomeArray[Math.floor(Math.random() * welcomeArray.length)]}\nJoin us in our daily Zoom sessions found under the #online-activities channel.`;
+  }
 };
